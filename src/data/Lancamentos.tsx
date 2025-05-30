78b714d385c392260d235462154d989a45fe6a30
bruce <euryanspike.pro@gmaiul.com>
2025-05-30 16:52:24 -0300

    dados mockados de donuts - lancamentos

---
+++ src/data/Lancamentos.tsx
@@ -0,0 +1,9 @@
+export const lancamentos = [
+        {
+            title: "Donuts de chocolate",
+            details: "Cobertura EXTRA de chocolate",
+            price: 8.99,
+            promotion: 3.99,
+            image: "/donuts_coberturachocolate.png",
+        },
+    ];
