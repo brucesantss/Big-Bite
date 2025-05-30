6269e8ac61470c00534cda4fe45203d10c19bd1c
bruce <euryanspike.pro@gmaiul.com>
2025-05-30 16:52:04 -0300

    dados mockados de donuts

---
+++ src/data/Donuts.tsx
@@ -0,0 +1,27 @@
+export const donuts = [
+
+    {
+        title: "Mini Donuts no Copo",
+        details: "Imagine um copo decorado com uma variedade de donuts coloridos e tentadores, cuidadosamente arrumados. O copo é feito de plástico resistente, com um design moderno e transparente, para destacar a beleza das delícias que ele contém. Dentro do copo, os donuts são variados em forma e sabor: há donuts radicionais com glacê de chocolate, outros com cobertura de frutas vermelhas, e alguns com granulado colorido ou até mesmo recheados com cremes irresistíveis.",
+        price: 24.90,
+        promotion: 19.90,
+        image: "minidonuts_nocopo.png",
+    },
+    {
+        title: "Donuts Classico",
+        details: "O donut clássico é a escolha perfeita para quem aprecia um sabor tradicional e irresistível.Ele possui uma massa macia e leve, levemente dourada por fora, com um toque suave e aerado no interior. A cobertura é um simples glacê rosa , cremoso e doce, que brilha sob a luz,dando-lhe um acabamento perfeito junto com granulados coloridos.",
+        price: 9.90,
+        promotion: 5.00,
+        image: "donuts_classico.png",
+    },
+    {
+        title: "Donuts Oreo",
+        details: "O donut clássico é a escolha perfeita para quem aprecia um sabor tradicional e irresistível.Ele possui uma massa macia e leve, levemente dourada por fora, com um toque suave e aerado no interior. A cobertura é um simples glacê rosa , cremoso e doce, que brilha sob a luz,dando-lhe um acabamento perfeito junto com granulados coloridos.",
+        price: 12.90,
+        promotion: 6.00,
+        image: "donuts_oreo.png",
+    },
+
+];
+
+
